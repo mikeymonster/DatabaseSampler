883329a91104875c5648f57247070368bda935f9
Mike Wild <wildconsultingltd@googlemail.com>
2021-03-22 11:59:29 +0000

    Change to top-level programs

--- DatabaseSampler.Functions/Program.cs
+++ DatabaseSampler.Functions/Program.cs
@@ -1,49 +1,35 @@
 ﻿using System;
-using System.Threading.Tasks;
 using DatabaseSampler.Application.Data;
 using DatabaseSampler.Application.Interfaces;
 using DatabaseSampler.Application.Services;
-//using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Npgsql;
 
-namespace DatabaseSampler.Functions
-{
-    public class Program
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .ConfigureServices(s =>
-                {
-                    //s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
-                    s.AddTransient<IPostgresSqlRepository, PostgresSqlRepository>();
-                    s.AddTransient<IPostgresSqlService, PostgresSqlService>();
-
-                    //s.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        s.AddTransient<IPostgresSqlRepository, PostgresSqlRepository>();
+        s.AddTransient<IPostgresSqlService, PostgresSqlService>();
 
                     //Configure PostgresSql
                     var pgConnectionString = Environment.GetEnvironmentVariable("PostgreSqlConnectionString");
-                    var pgPassword = Environment.GetEnvironmentVariable("PostgreSqlDbPassword");
-                    var pgBuilder = new NpgsqlConnectionStringBuilder(pgConnectionString)
-                    {
-                        Password = pgPassword
-                    };
+        var pgPassword = Environment.GetEnvironmentVariable("PostgreSqlDbPassword");
+        var pgBuilder = new NpgsqlConnectionStringBuilder(pgConnectionString)
+        {
+            Password = pgPassword
+        };
 
-                    s.AddDbContext<StudentDbContext>(options =>
-                        options.UseNpgsql(pgBuilder.ConnectionString));
+        s.AddDbContext<StudentDbContext>(options =>
+            options.UseNpgsql(pgBuilder.ConnectionString));
 
-                    Console.WriteLine("Adding HTTP Client");
-                    s.AddHttpClient<ILocationService, LocationService>();
+        Console.WriteLine("Adding HTTP Client");
+        s.AddHttpClient<ILocationService, LocationService>();
 
-                    s.AddTransient<ILocationService, LocationService>();
-                })
-                .Build();
+        s.AddTransient<ILocationService, LocationService>();
+    })
+    .Build();
 
-            await host.RunAsync();
-        }
-    }
-}
+await host.RunAsync();
--- DatabaseSampler/Program.cs
+++ DatabaseSampler/Program.cs
@@ -1,20 +1,9 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using DatabaseSampler;
 
-namespace DatabaseSampler
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+CreateWebHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
